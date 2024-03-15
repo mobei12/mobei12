db124870b945c46847f9a1dcebddcab05d94efc3
mobei12 <505453856@qq.com>
2024-03-15 23:03:20 +0800

    1、修改build方式 2,完善页面

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,6 +1,10 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.base');
 const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.base.js');
-const defConfig = merge(baseConfig, {
+const PORT = 8080; // 你的 devServer 端口号
+const URL = `http://localhost:${PORT}/#/home`; // 要打开的 URL
+const defConfig = merge(config, {
 	mode: 'development',
 	devtool: 'source-map',//开启sourceMap，方便调试
 	devServer: {
@@ -14,4 +18,21 @@ const defConfig = merge(baseConfig, {
 		],
 	},
 });
-module.exports = defConfig;
+// 创建 webpack compiler
+const compiler = webpack(defConfig);
+// 创建 devServer
+const devServerOptions = Object.assign({}, defConfig?.devServer, {
+	/*open: true, // 打开浏览器
+	 hot: true,*/
+});
+const server = new WebpackDevServer(devServerOptions, compiler);
+// 启动 devServer
+server.start().then(() => {
+	console.log(`dev server listening on port ${PORT}`);
+
+	// 打开指定路由
+	const { exec } = require('child_process');
+	exec(`start ${URL}`); // 在 Windows 上打开 URL
+}).catch((err) => {
+	console.error(err);
+});
