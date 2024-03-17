a0efe8cfaeda5dde697ac68cb0c6156d636128a8
mobei12 <505453856@qq.com>
2024-03-17 22:52:47 +0800

    1.缓存配置 2.修改404页面

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,17 @@
 import { FC } from 'react';
+import { Link} from 'react-router-dom';
 
 const NoFoundPage: FC = () => {
-	const goBack = (): void => {
-		// Todo 修改路由切换
-		window.location.href = '/#/home/OverView';
-	};
 	return (
-		<div className='text-center pt-20 space-y-10'>
-			<h2 className='text-5xl'>404</h2>
-			<div className=' bg-sky-500 w-20  p-1 mx-auto rounded-md cursor-pointer hover:bg-sky-700 text-gray-50' onClick={goBack}>
-				去主页
+		<div className="text-center pt-20 space-y-10">
+			<h2 className="text-5xl">404</h2>
+			<div
+				className=" bg-sky-500 w-20  p-1 mx-auto rounded-md cursor-pointer hover:bg-sky-700 text-gray-50"
+			>
+				<Link to="/home">Home</Link>
 			</div>
-			<p className='text-gray-400'>这里什么 都没有</p>
-			<p className='text-gray-400 mt-20'>design by @mobei</p>
+			<p className="text-gray-400">这里什么 都没有</p>
+			<p className="text-gray-400 mt-20">design by @mobei</p>
 		</div>
 	);
 };
