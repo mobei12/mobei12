7525404cdba8d757bc5078ce3ec4c15fa83c56f7
mobei12 <505453856@qq.com>
2024-03-31 22:37:04 +0800

    1，重构tag标签组件 2，完善个别文章页面

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,47 +1,39 @@
+const tagS = {
+    'react': 'react',
+    'vue': 'Vue.js',
+    'webpack': 'Webpack',
+    'node': 'nodedotjs',
+    'express': 'express',
+    'git': 'git',
+    'javascript': 'javascript',
+    'html5': 'HTML5',
+    'css': 'CSS3',
+    'typescript': 'TypeScript',
+    'tailwindcss': 'tailwindcss',
+    'bootstrap': 'Bootstrap',
+    'vite': 'vite',
+    'element': 'element',
+    'antdesign': 'AntDesign',
+    'echarts': 'Apache%20ECharts',
+    'mongodb': 'MongoDB',
+    'subversion': 'subversion',
+    'env': '.env',
+    'reactrouter': 'reactrouter'
+};
+
+/**
+ * @description 根据输入的标签生成URL。
+ *
+ * @param {string} tag - 要生成URL的标签。
+ * @return {string} 根据标签生成的URL。
+ * @notice  请注意，标签名称不区分大小写,tagS需要在线查找 https://shields.io/badges
+ */
 const getUrl = (tag) => {
-    switch(tag.trim().toLowerCase()) {
-        case 'react':
-            return 'https://img.shields.io/badge/-React-333333?style=flat&logo=react'
-        case 'vue':
-            return 'https://img.shields.io/badge/-Vue-333?style=flat&logo=Vue.js'
-        case 'webpack':
-            return 'https://img.shields.io/badge/-Webpack-333?style=flat&logo=Webpack'
-        case 'node':
-            return 'https://img.shields.io/badge/-Node-333?style=flat&logo=nodedotjs'
-        case 'express':
-            return 'https://img.shields.io/badge/-express-333?logo=express'
-        case 'git':
-            return 'https://img.shields.io/badge/-Git-333333?style=flat&logo=git'
-        case 'javascript':
-            return 'https://img.shields.io/badge/-JavaScript-333333?style=flat&logo=javascript'
-        case 'html5':
-            return 'https://img.shields.io/badge/-HTML5-333333?style=flat&logo=HTML5'
-        case 'css':
-            return 'https://img.shields.io/badge/-CSS-333333?style=flat&logo=CSS3'
-        case 'typescript':
-            return 'https://img.shields.io/badge/-TypeScript-333?style=flat&logo=TypeScript'
-        case 'tailwindcss':
-            return 'https://img.shields.io/badge/-tailwindcss-333?style=flat&logo=tailwindcss'
-        case 'bootstrap':
-            return 'https://img.shields.io/badge/-Bootstrap-333?style=flat&logo=Bootstrap'
-        case 'vite':
-            return 'https://img.shields.io/badge/-Vite-333?style=flat&logo=vite'
-        case 'element':
-            return 'https://img.shields.io/badge/-element-333?style=flat&logo=element'
-        case 'antdesign':
-            return 'https://img.shields.io/badge/-AntDesign-333?style=flat&logo=AntDesign'
-        case 'echarts':
-            return 'https://img.shields.io/badge/-%20ECharts-333?style=flat&logo=Apache%20ECharts'
-        case 'mongodb':
-            return 'https://img.shields.io/badge/-MongoDB-333?style=flat&logo=MongoDB'
-        case 'subversion':
-            return 'https://img.shields.io/badge/-Subversion-333?style=flat&logo=subversion'
-        case 'env':
-            return 'https://img.shields.io/badge/-env-333?logo=.env'
-        case 'reactrouter':
-            return 'https://img.shields.io/badge/-RreactRouter-333?logo=ReactRouter'
-        default:
-            return `https://img.shields.io/badge/-${tag}-333333?style=flat`
+    const logoVal = tagS[tag.trim().toLowerCase()]
+    if(!logoVal) {
+        return `https://img.shields.io/badge/-${tag}-333?style=flat`
+    }else {
+        return `https://img.shields.io/badge/-${tag}-333?logo=${logoVal}`
     }
 }
 const Tag = ({ tag }) => {
