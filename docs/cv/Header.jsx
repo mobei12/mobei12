1bd0998d98233925a23b33cc9da8ccce83390618
mobei12 <505453856@qq.com>
2024-04-08 15:06:16 +0800

    完善cv适配

--- docs/cv/Header.jsx
+++ docs/cv/Header.jsx
@@ -2,8 +2,11 @@ import Tag from '../../src/components/Tag'
 import img from '../public/mobei12.jpg'
 const Header = () => {
     return (
-        <div style={{display: 'flex', }}>
-            <div style={{flex:1}}>
+        <div className=' flex  flex-col md:flex-row max-w-6xl min-h-[35vh] mt-12 sm:mt-0 justify-between'>
+            <div className='flex flex-col justify-center  max-w-xl sm:max-w-3xl order-2 md:order-1'>
+                <div>
+                    <Tag tag='Info' />：蒋军晖/男/1992
+                </div>
                 <div>
                     <Tag tag='Email' />：mobeigege@icloud.com
                 </div>
@@ -11,7 +14,9 @@ const Header = () => {
                     <Tag tag='WeChat' />DEBUG__LIFE
                 </div>
             </div>
-            <img src={img} alt="mobei" width="250" height="100%"/>
+            <div className='user-img order-1 '>
+                <img src={img}  alt="mobei" width="300" height="100"/>
+            </div>
         </div>
     )
 }
