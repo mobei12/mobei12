8ee4af0bd48e885f1f53928a41c283eadf9eabc8
mobei12 <505453856@qq.com>
2024-04-12 16:19:16 +0800

    更新cv

--- docs/cv/Header.jsx
+++ docs/cv/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <div className=' flex  flex-col md:flex-row max-w-6xl min-h-[35vh] mt-12 sm:mt-0 justify-between'>
             <div className='flex flex-col justify-center  max-w-xl sm:max-w-3xl order-2 md:order-1'>
                 <div>
-                    <Tag tag='Info' />：蒋军晖/男/1992
+                    <Tag tag='Info' />：蒋军晖/男
                 </div>
                 <div>
                     <Tag tag='Email' />：mobeigege@icloud.com
